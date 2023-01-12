afcf9782917477b35e72f041462c42fd3ead8965
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-11 21:22:08 -0600

    finished writing out the code for dropping and creating tables, currently bugged

--- db/seed.js
+++ db/seed.js
@@ -1,11 +1,46 @@
+const { create } = require('domain');
 const { client, getAllUsers } = require('./index');
 
-async function testDB() {
+// async function testDB() {
+//   try {
+//     client.connect();
+//     // const { rows } = await client.query(`SELECT * FROM users`);
+//     const users = await getAllUsers();
+//     console.log(users);
+//   } catch (err) {
+//     console.error(err);
+//   } finally {
+//     client.end();
+//   }
+// }
+
+// function should call a query which drops all tables from our db
+async function dropTables() {
+  try {
+    await client.query(`DELETE * FROM users`);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// function should call a query which creates all tables for our db
+async function createTables() {
+  try {
+    await client.query(`
+    INSERT users (id, username, password) 
+    VALUES ($1, $2, $3)
+    RETURNING *`);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// function should call a query which rebuilds the db
+async function rebuildDB() {
   try {
     client.connect();
-    // const { rows } = await client.query(`SELECT * FROM users`);
-    const users = await getAllUsers();
-    console.log(users);
+    await dropTables();
+    await createTables();
   } catch (err) {
     console.error(err);
   } finally {
@@ -13,12 +48,5 @@ async function testDB() {
   }
 }
 
-async function dropTables() {
-  try {
-    await client.query(``);
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-testDB();
+// testDB();
+rebuildDB();
