57855f3c429dcb97a1643e078ae9c3d8e7cd8776
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-15 01:45:20 -0600

    finished create user in part 2

--- api/users.js
+++ api/users.js
@@ -3,7 +3,7 @@ const userRouter = express.Router();
 
 const jwt = require('jsonwebtoken');
 
-const { getAllUsers, getUserByUsername } = require('../db');
+const { getAllUsers, getUserByUsername, createUser } = require('../db');
 
 userRouter.use((req, res, next) => {
   console.log('A request is being made to /users');
@@ -45,6 +45,46 @@ userRouter.post('/login', async (req, res, next) => {
   } catch (err) {
     console.log(err);
     next(err);
+  }
+});
+
+userRouter.post('/register', async (req, res, next) => {
+  const { username, password, name, location } = req.body;
+
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: 'UserExistsError',
+        message: 'A user by that username already exists',
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+      name,
+      location,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '1w',
+      }
+    );
+
+    res.send({
+      message: 'thank you for signing up',
+      token,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 });
 
