24055847c5f14bdd65ac9b521b6c1b7506067ed0
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-15 01:09:43 -0600

    setting up jwt login authorization

--- api/users.js
+++ api/users.js
@@ -17,4 +17,9 @@ userRouter.get('/', async (req, res) => {
   });
 });
 
+userRouter.post('/login', async (req, res, next) => {
+  console.log(req.body);
+  res.end();
+});
+
 module.exports = userRouter;
