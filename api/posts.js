12cbee378a821c6a4e6a81b10b0ddd2283cb6a9e
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-17 23:13:58 -0600

    ability to create a post works

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const postRouter = express.Router();
 
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost } = require('../db');
 const { requireUser } = require('./utils');
 
 // post logger
@@ -11,9 +11,32 @@ postRouter.use((req, res, next) => {
   next();
 });
 
-// post with requireUser
+// Create post with requireUser
 postRouter.post('/', requireUser, async (req, res, next) => {
-  res.send({ message: 'under construction' });
+  const { title, content, tags = '' } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    postData.title = title;
+    postData.content = content;
+    postData.authorId = req.user.id;
+
+    const post = await createPost(postData);
+
+    if (post) {
+      res.send({
+        post,
+      });
+    }
+  } catch ({ name, username }) {
+    next({ name, username });
+  }
 });
 
 // get all posts
