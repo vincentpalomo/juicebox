ec0fd3b1d07f881184621a5f8a1c5d300215daff
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-18 21:58:21 -0600

    added filter to tagName router

--- api/tags.js
+++ api/tags.js
@@ -25,7 +25,10 @@ tagRouter.get('/:tagName/posts', async (req, res, next) => {
   const tagName = req.params.tagName;
 
   try {
-    const posts = await getPostsByTagName(tagName);
+    const allPosts = await getPostsByTagName(tagName);
+    const posts = allPosts.filter((post) => {
+      return post.active || (req.user && post.author.id === req.user.id);
+    });
     res.send({ posts });
   } catch ({ name, message }) {
     next({ name, message });
